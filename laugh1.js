677d97354b9c5bd0528e85a2cdeb9f7072d89280
bill <billmukenga01@gmail.com>
2023-03-03 12:00:35 +0300

    solution for laugh quiz

---
+++ laugh1.js
@@ -0,0 +1,19 @@
+/*
+ * Programming Quiz: Laugh it Off 1
+ */
+
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should have a `laugh()` function
+ * - Your `laugh()` function should return the correct output
+ * - Your code should print `\"hahahahahahahahahaha!\"` by calling the `laugh()`
+ */
+
+// your code goes here
+function laugh(){
+    message = "hahahahahahahahahaha!";
+    return message;
+};
+
+// test your code by logging out the returned value
+console.log(laugh());
