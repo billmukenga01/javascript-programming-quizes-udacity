ce97496b7908c49ed790aae0fb993bd7f9cb2e4f
bill <billmukenga01@gmail.com>
2023-03-14 18:05:48 +0300

    solution

--- rainbow.js
+++ rainbow.js
@@ -11,4 +11,4 @@ const rainbow = ['Red', 'Orange', 'Blackberry', 'Blue'];
 
 // your code goes here
 rainbow.splice(2,1,'Yellow','Green');
-rainbow.splice(-1,1,'Yellow')
+rainbow.splice(5,0,'Yellow')
