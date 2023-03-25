57abfec31eec27558031c6a3564499aaa5776f18
billmukenga01 <billmukenga01@gmail.com>
2023-03-25 12:36:24 +0300

    fix: removed bugs that generated undifined

--- auto-password-generator.js
+++ auto-password-generator.js
@@ -11,13 +11,15 @@ const passwordLength = prompt('Input desired password length', 6);
 
 const generatedString = [];
 
+
+
 if (generatedString.length < passwordLength){
     
-    for(i=passwordLength; i>=0; i--){
-        generatedString.push(upperCase[Math.round(Math.random()*26)])
-        generatedString.push(lowerCase[Math.round(Math.random()*26)])
-        generatedString.push(specialCharacters[Math.round(Math.random()*9)])     
-        generatedString.push(Math.round(Math.random()*10))
+    for(i=passwordLength; i>0; i--){
+        generatedString.push(upperCase[Math.floor(Math.random()*26)])
+        generatedString.push(lowerCase[Math.floor(Math.random()*26)])
+        generatedString.push(specialCharacters[Math.floor(Math.random()*9)])     
+        generatedString.push(Math.floor(Math.random()*10))
     }
     
 }
@@ -27,6 +29,26 @@ if(generatedString.length > passwordLength){
 }
 
 
+function getRandomInt(n) {
+    return Math.floor(Math.random() * n);
+  }
+function shuffle(array) {
+            // Convert String to array
+    let n = array.length ;              // Length of the array
+    
+    for(var i=0 ; i<n-1 ; ++i) {
+      let j = getRandomInt(n);       // Get random of [0, n-1]
+      
+      let temp = array[i];             // Swap arr[i] and arr[j]
+      array[i] = array[j];
+      array[j] = temp;
+    }
+    
+    password = array.join('');                // Convert Array to string
+    return password;                        // Return shuffled string
+}
+
+let shuffledPassword = shuffle(generatedString);
 
 console.log(upperCase);
 console.log(lowerCase);
