db4e2cb1c3a5a1589c8ac55b972b17e4a12ed865
billmukenga01 <billmukenga01@gmail.com>
2023-03-25 12:22:41 +0300

    added logic to generate an array from given arrays

--- auto-password-generator.js
+++ auto-password-generator.js
@@ -5,11 +5,29 @@ const lowerCase = upperCase.map(input => input.toLowerCase());
 
 const specialCharacters = ['!', '@','#','$','&','(',')','%','*'];
 
-const randomNumber = Math.round(Math.random()*10);
+//const randomNumber = Math.round(Math.random()*10);
 
 const passwordLength = prompt('Input desired password length', 6);
+
+const generatedString = [];
+
+if (generatedString.length < passwordLength){
+    
+    for(i=passwordLength; i>=0; i--){
+        generatedString.push(upperCase[Math.round(Math.random()*26)])
+        generatedString.push(lowerCase[Math.round(Math.random()*26)])
+        generatedString.push(specialCharacters[Math.round(Math.random()*9)])     
+        generatedString.push(Math.round(Math.random()*10))
+    }
+    
+}
+
+if(generatedString.length > passwordLength){
+    generatedString.splice(-(generatedString.length - passwordLength), generatedString.length - passwordLength)
+}
+
 
 
 console.log(upperCase);
 console.log(lowerCase);
-console.log(randomNumber);
+console.log(generatedString);
