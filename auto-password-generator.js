d277f90d11d240e496cbbd693c20c6881c24e97a
billmukenga01 <billmukenga01@gmail.com>
2023-03-25 11:19:12 +0300

    declare uppercase and lowercase variables

---
+++ auto-password-generator.js
@@ -0,0 +1,7 @@
+const upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M',
+                   'N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+
+const lowerCase = upperCase.forEach(element => {
+    element.toLowerCase;
+});
+
