a8d1019278be04b9df2ad80dff583a4b5267e0ca
billmukenga01 <billmukenga01@gmail.com>
2023-04-03 18:31:20 +0300

    a new div with 2 children

--- JS-DOM/script.js
+++ JS-DOM/script.js
@@ -8,6 +8,16 @@ body.appendChild(p);
 
 //h3 with some blue text in it
 const h3 = document.createElement('h3');
-p.textContent = ' I\'m a blue h3!';
-p.style.cssText = 'color:blue';
-body.appendChild(p);
+p1.textContent = ' I\'m a blue h3!';
+p1.style.cssText = 'color:blue';
+body.appendChild(p1);
+
+//a new div with different styling
+const div = document.createElement('div');
+const h1 = document.createElement('h1');
+const p2 = document.createElement('p');
+
+p2.textContent = ' MEE TOO!';
+h1.textContent = ' I\'m in a div ';
+div.appendChild(h1);
+div.appendChild(p2);
